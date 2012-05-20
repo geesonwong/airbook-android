eaaac85d597e60c08f22910260185c343cca241d
airsen <jason61719@gmail.com>
2012-05-20 11:59:08 +0800

    first commit

---
+++ src/info/airbook/AirbookActivity.java
@@ -0,0 +1,13 @@
+package info.airbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AirbookActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
